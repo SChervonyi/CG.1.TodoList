e9910ea096c0e3eeb6a9149b6ee30432ab7982df
TatradymovaIrina <tatrira516@gmail.com>
2016-08-04 22:10:39 +0300

    Added function which write user's text in console

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function ShowInput()
-{
-    console.log("Hello, Sergey!");
+function ShowInput() {
+    var x, text;
+    x = document.getElementById("add_field").value;
+    if (isNaN(x)) {
+        text = x;
+    } else {
+        text = "Enter text, please";
+    }
+    console.log(text);
 }
