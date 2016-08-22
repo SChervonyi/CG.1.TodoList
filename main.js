7d6315d967a61bf4a5bc89078c739acb6b79c7b6
TatradymovaIrina <tatrira516@gmail.com>
2016-08-22 21:19:09 +0300

    It is program with needed idea, but I could not add the TodoView and BindToController(I don't understand still this thing). I am stuck

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-function ShowInput() {
-    var x, text;
-    x = document.getElementById("add_field").value;
-    if (isNaN(x)) {
-        text = x;
-    } else {
-        text = "Enter text, please";
-    }
-    console.log(text);
-}
+var app = angular.module("myToDoList", []); 
+app.controller("myCtrl", function($scope) {
+  $scope.products = ["Hello!"];
+  $scope.addRecord = function () {
+   $scope.errortext = "";
+   if (!$scope.addRec) {return;}        
+   if ($scope.products.indexOf($scope.addRec) == -1) {
+    $scope.products.push($scope.addRec);
+   } else {
+    $scope.errortext = "Attention. Record exists!";
+   }
+  }
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.todoInput = "";
+  $scope.removeRecord = function (x) {
+   $scope.errortext = "";    
+   $scope.products.splice(x, 1);
+  }
 });
