7d950c9ce79fd2ab2d5a2da2d75b630d13bbe2b2
Sergii Chervonyi <Sergey.Chervonyi@mandosys.com>
2016-08-24 18:24:43 +0300

    Update code structure

---
+++ src/app/main.js
@@ -0,0 +1,22 @@
+var app = angular.module("myToDoList", []); 
+
+app.controller("myCtrl", function($scope) {
+    $scope.products = ["Hello!"];
+
+    $scope.addRecord = function () {
+        $scope.errortext = "";
+
+        if (!$scope.addRec) return;
+
+        if ($scope.products.indexOf($scope.addRec) == -1) {
+            $scope.products.push($scope.addRec);
+        } else {
+            $scope.errortext = "Attention. Record exists!";
+        }
+    }
+
+    $scope.removeRecord = function (x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    }
+});
